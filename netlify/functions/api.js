04547453d0be63e14e8bc1b2cc028190f71d9b4b
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-01 16:26:46 +0545

    build: netlify functions configured using docs

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,14 +1,25 @@
 const express = require("express"); 
 const app = express(); 
 const mongoose = require("mongoose"); 
-const postRoute = require("./routes/post"); 
-const commentRoute = require("./routes/comment");
-const getPostsRoute = require("./routes/getPosts"); 
-const getCommentsRoute = require("./routes/getComments"); 
+const postRoute = require("../../routes/post"); 
+const commentRoute = require("../../routes/comment");
+const getPostsRoute = require("../../routes/getPosts"); 
+const getCommentsRoute = require("../../routes/getComments"); 
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors= require('cors')
 const dotenv = require("dotenv"); 
+import serverless from 'serverless-http'; 
+
+//netlify function
+
+// exports.handler = async function (event , context) { 
+//     return { 
+//         statusCode: 200, 
+//         body: JSON.stringify({message: "Hello World"}) , 
+//     };
+// };
+
 
 dotenv.config(); 
 mongoose.set('strictQuery', true);
@@ -23,6 +34,10 @@ app.use("/api/getPosts", getPostsRoute);
 app.use("/api/comment", commentRoute); 
 app.use("/api/getComments", getCommentsRoute);
 
+
+
 app.listen(8800, () => { 
     console.log ("Backend server is running at port 8000"); 
 })
+
+export const handler = serverless(app); 
