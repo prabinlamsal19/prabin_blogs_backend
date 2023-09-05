914fd1e13877a65e9e2c0d3efa209bc837f44afa
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-05 09:26:46 +0545

    build; cors config detailed

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -15,7 +15,21 @@ const getTestsRoute = require("../../routes/getTest");
 
 exports.handler = function async(event , context) { 
     app.use(cors()); 
-    app.options('*', cors());
+    app.options('*', cors()); 
+    app.use(function (req, res, next) {
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+      
+      
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      
+      
+        res.setHeader('Access-Control-Allow-Credentials', true);
+      
+        next();
+      });
     dotenv.config(); 
     mongoose.set('strictQuery', true);
     mongoose.connect( 
