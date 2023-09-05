156ff7ab088f5dfeb673a8ca89edef1ca24d0d6b
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-05 10:04:14 +0545

    build: added env var in netlify itself

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -17,24 +17,15 @@ exports.handler = function async(event , context) {
     app.use(cors()); 
     app.options('*', cors()); 
     app.use(function (req, res, next) {
-
         res.setHeader('Access-Control-Allow-Origin', '*');
-      
-      
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-      
-      
         res.setHeader('Access-Control-Allow-Credentials', true);
-      
         next();
       });
     dotenv.config(); 
     mongoose.set('strictQuery', true);
-    mongoose.connect( 
-        process.env.MONGO_URL)
- 
+    mongoose.connect(process.env.MONGO_URL)
 
     app.use(express.json());
     app.use("/api/post", postRoute);
